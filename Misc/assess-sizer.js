76b2657e9550b734be201cf2bf3643ee602b2096
resource11 <mcmahon@resource11.com>
2017-11-28 16:22:24 -0500

    initial commit

---
+++ Misc/assess-sizer.js
@@ -0,0 +1,14 @@
+'use strict';
+
+$(document).ready(function() {
+  window.addEventListener("message", function(event){
+    var player = document.getElementById('openassessments_container')
+    var player = document.getElementsByClassName('openassessments_container')
+    var payload = JSON.parse(event.data);
+    if(payload.height) {
+      player.style.height = payload.height + 'px';
+    }
+
+  }, false);
+
+});
--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # ART-transcript-patterns
+
+Updated UI patterns for Audio Record Tool layouts, and a generic UI pattern for video layouts leveraging VideoJS with transcripts support
