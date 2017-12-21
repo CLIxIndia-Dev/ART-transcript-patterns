25b3c6087871ce4c9efd51ed080fa21d9be53a6c
resource11 <mcmahon@resource11.com>
2017-12-21 13:14:34 -0500

    adds focus styles and handlers for transcript toggle, option select buttons

--- Misc/transcript-toggle.js
+++ Misc/transcript-toggle.js
@@ -1,7 +1,12 @@
 const toggle = () => {
+  const classes = {
+    containerHasFocus: 'container--has-focus'
+  };
   const mediaNavs = document.querySelectorAll('.media-nav')
 
   Array.prototype.forEach.call(mediaNavs, mediaNav => {
+
+    let art = mediaNav.querySelector('.audio-select')
     let btn = mediaNav.querySelector('.transcript-toggle')
     let target = mediaNav.nextElementSibling
 
@@ -11,7 +16,27 @@ const toggle = () => {
       btn.setAttribute('aria-expanded', !expanded)
       target.hidden = expanded
     }
+
+    // Add the focus class to the container if the keyboard
+    // event is an element within the container
+    document.addEventListener('keyup', (e) => {
+        if (mediaNav.contains(e.target)) {
+            mediaNav.classList.add(classes.containerHasFocus);
+        } else {
+            mediaNav.classList.remove(classes.containerHasFocus);
+        }
+    });
+
+    // Remove the focus class on mouse click
+    document.addEventListener('mousedown', (e) => {
+        if (mediaNav.contains(e.target)) {
+            mediaNav.classList.remove(classes.containerHasFocus);
+        }
+    });
+
   })
+
+
 }
 
 window.onload = toggle;
